861da96a70d71f9950cf2e82cbd972fb91ef9149
Md. Abul Kalam Azad <akalamazad208@gmail.com>
2026-08-01 22:55:15 +0600

    Topic 12 (Part 4): Styling Navigation Bar with CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav>
+    <nav className='navbar'>
       <Link to = "/">Home</Link> {" | "}
-      <Link to = "/about">About</Link> {"  "}
+      <Link to = "/about">About</Link> {" | "}
       <Link to = "/services">Services</Link> {" | "}
       <Link to = "/contact">Contact</Link>
     </nav>
