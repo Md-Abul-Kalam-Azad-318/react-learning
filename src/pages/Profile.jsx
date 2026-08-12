63b7329df459f070bec341511cfd3783db48c729
Md. Abul Kalam Azad <akalamazad208@gmail.com>
2026-08-12 08:50:56 +0600

    Topic 12 (Part 11): useNavigate and useLocation

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import {useNavigate } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 
 function Profile() {
-  return <h2>Profile Page</h2>;
+
+    const navigate = useNavigate();
+    // const location = useLocation();
+
+  return (
+    <div>
+      <h2>Profile Page</h2>
+
+      {/* <button onClick={() =>navigate("/dashboard")}>
+        Back to Dashboard
+      </button> */}
+
+      {/* <button onClick={() => navigate(-1)}>
+        Go Back
+      </button> */}
+
+      <button onClick={() =>navigate("/dashboard", 
+      {replace : true })}>
+        Back to Dashboard
+      </button>
+      {/* <p>Current path: {location.pathname}</p> */}
+
+      {/* <p>Path: {location.pathname}</p>
+      <p>Search: {location.search}</p>
+      <p>Hash: {location.hash}</p>
+     */}
+    </div>
+  );
+  
 }
 
 export default Profile;
